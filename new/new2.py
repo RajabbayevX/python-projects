d1138c188c67e2effeab8675ae572323ef50805f
Khayitboy_Rajabbayev <125670810+RajabbayevX@users.noreply.github.com>
2023-02-18 23:14:26 +0500

    yana yangi narsa
    2-darsim

---
+++ new/new2.py
@@ -0,0 +1,260 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+#avtolar = ["bmw", "audi", "volvo", "chevrolet"]
+
+#for avto in avtolar:
+ #   if avto =="bmw" :
+  #      print(avto.upper())
+   # else:
+    #    print(avto.title())
+    
+#ism = "Ali"
+#ism.lower() == 'ali'
+#ism  = input("Ismingiz nima?\n>>>")
+#if ism.lower() != "ali":
+ #   print(f"Uzr, {ism.title()} biz Alini kutyapmiz.")
+    
+#else:
+  #   print("Salom Ali")
+
+#javob = float(input("12x6 nechiga teng?>>>"))
+#if javob !=72:
+ #   print("Javob xato!")
+ 
+#yosh = int(input("Yoshingiz nechida?>>>"))
+#if yosh>=18:
+#    print("Xush kelibsiz")
+#else:
+ #   print("KIrish mumkin emas")
+#login = input("Yangi login tanlang:")
+#if len(login)<=5:
+ #   print("Login 5 harfdan ko'ptoq bo'lishi kerak")
+#yil = int(input("Tug'ilgan yilingizni kiriting:"))
+#if 2023-yil<18:
+ #   print(f"Yoshingiz {2023-yil}da ekan")
+  #  print("Kirish mumkin emas")
+#else:
+ #   print("Xush kelibsiz")
+ 
+#yosh = int(input("Yoshingiz nechida?>>>"))
+#if yosh>65:  print("Siz COVID-19 risk guruhida ekansiz!") 
+ 
+ 
+#x, y = 25, 50
+#print("x>y")if x>y else print("x<y")
+#cars =["toyota", "gm", "mazda", "hyundai", "kia"]
+#for car in cars:
+ #   if car =="gm":
+  #      print(car.upper())
+   # else:
+    #    print(car.title())
+    
+#cars =["toyota", "gm", "mazda", "hyundai", "kia"]
+#for car in cars:
+ #   if car!="gm":
+  #      print(car.title())
+   # else:
+    #     print(car.upper())
+#login = input("Login kiriting: ")
+#if login.lower() == 'admin':
+ #   print("Xush kelibsiz Admin, foydalanuvchilar ro'yhatini ko'rasizmi?")
+#else:
+ #   print(f"Xush kelibsiz{login.title()}!")
+
+#x = float(input("Birinchi sonni kiriting"))
+#y = float(input("Ikkinchi sonni kiriting"))
+#if x==y: print(f"sonlar teng: {x}={y}")
+
+
+#son = float(input("Istalgan sonni kiriting: "))
+#print("Son manfiy") if son<0 else print("Son musbat")
+
+
+#son = float(input("Istalgan sonni kiriting: "))
+#print(son**(1/2)) if son>0 else print("Musbat sonni kiriting")
+
+
+#son = -23
+
+#if son<0:
+#    print("Manfiy son")
+#else:
+ #     print("Musbat son")
+
+#yosh = int(input("Yoshingiz nechida? "))
+#if yosh<=4:
+#   narh =0
+#elif yosh<=12:
+ #   narh = 5000
+#elif yosh<=18:
+#    narh = 8000
+#else:
+ #   narh = 10000
+
+#print(f"Sizga kirish {narh} so'm")
+
+#kun = input("Bugun qaysi kun?>>>")
+#if kun.lower()=='shanba' or kun.lower()=='yakshanba':
+ #   print("Bugun dam olish kuni!")
+#else:
+#    print("Bugun ish kuni")
+
+#kun = input("Bugun qaysi kun?>>>")
+#harorat = float(input("Havo harorati qanday?>>>"))
+#if kun.lower()=='yakshanba' or kun.lower()=='shanba' and harorat>=30:
+ #   print("Cho'milgani ketdik")
+#elif kun.lower()=='yakshanba' or kun.lower()=='shanba' and harorat<30:
+ #   print("Uyda dam olamiz") 
+
+#narh = 15000
+#choy = True
+#salat = True
+#if choy and salat:
+#    narh = narh+10000
+#elif choy or salat:
+ #   narh = narh+5000
+
+#print(f"Jami {narh} so'm")    
+
+
+
+narh = 15000
+choy = 0
+salat = 1
+non = 1
+kompot = 1
+assorti = 0
+#if choy:
+#    print("MIjoz choy oldi")
+#    narh = narh+3000
+##if salat:
+#   print("Mijoz salat oldi")
+#   narh = narh+5000
+#if non:
+#   print("Mijoz non oldi")
+#   narh = narh+5000
+##   print("Mijoz kompot oldi")    
+ ## assorti:
+  #  print("Mijoz assorti olsin")
+  #  narh = narh+15000
+  #  
+#print(f"Jami {narh} so'm")    
+
+#menu = ['osh', 'kabob', 'chuchvara', 'burger']
+#ovqat = input("Nima ovqat yeysiz?>>>")
+#if ovqat.lower () in menu:
+#    print("Buyurtma qabul qilindi!")
+#else:
+#    print("Afsuski bizda bunday taom yo'q!")    
+#menu = ['osh', 'kabob', 'chuchvara', 'burger']
+#buyurtmalar = ["somsa", "osh", "burger", "kabob", "lavash"]
+
+#for taom in buyurtmalar:
+#      if taom in menu:
+#          print(f"Menuda {taom} bor")
+#else:
+ #   print(f"Kechirasiz, menuda {taom} yo'q")        
+
+#son = float(input("Juft sonni kiriting>>>"))
+#if son%2:
+#    print("Bu son juft emas")
+#else:
+#    print("Rahmat")    
+
+#yosh = int(input("Yoshingiz nechada?>>>"))
+#if yosh<=4 or yosh>=60:
+#    narh = 0
+#elif yosh < 18:
+#    narh = 10000
+#else:
+#    narh = 20000
+#
+#print(f"Chipta {narh} so'm")    
+
+#yosh = int(input("Yoshingiz nechida?"))
+#
+#if yosh<=4 or yosh>=60:
+#    narh = 0
+#elif yosh < 18:
+#    narh = 10000
+#else:
+#    narh = 20000
+#print(f"Chipta {narh} so'm")
+
+#x = float(input("Birinchi sonni kiriting: "))
+#y = float(input("Ikkinchi sonni kiriting: "))
+#if x==y:
+#    print(f"{x}={y}")
+#elif x<y:
+    print(f"{x}<{y}")
+else:
+    print(f"{x}>{y}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
